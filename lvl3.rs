7f9f36551abbaa889f8e579d16cd9e0ff3f711f1
Julian Muhlberger <moewenmann@protonmail.ch>
2024-04-19 11:46:32 +0200

    .

--- lvl3.rs
+++ lvl3.rs
@@ -13,46 +13,37 @@ fn main() {
     // skip the first two lines
     let mut iter = reader.lines().skip(2);
 
-    let mut coins = Vec::new();
-
     let mut output_file =
         File::create(output_file_path.clone()).expect("Error creating output file");
 
     while let Some(line) = iter.next() {
         let line = line.expect("Error reading line");
-        coins.clear();
-        for coin in line.split_whitespace() {
-            coins.push(coin.parse::<i32>().expect("Error parsing coin"));
-        }
+        let coins: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().expect("Error parsing coin"))
+            .collect();
+        // Function to recursively find the fewest number of coins needed for an amount
 
-        // Vector to store the fewest number of coins needed for each amount from 1 to 100
-        let mut fewest_coins = vec![1000; 101]; // Initializing with a large number
-
-        // Base case: no coins needed for amount 0
-        fewest_coins[0] = 0;
-
-        for amount in 1..=100 {
-            // For each coin, check if it can be used to make up the current amount
-            for coin in &coins {
-                if *coin <= amount {
-                    // If using this coin reduces the number of coins needed for the current amount
-                    // update the fewest_coins array
-                    fewest_coins[amount as usize] = fewest_coins[amount as usize]
-                        .min(fewest_coins[(amount - coin) as usize] + 1);
+        // Call backtrack for each amount from 1 to 100
+        for mut amount in 1..=100 {
+            // loop over coins from back to front
+            for &coin in coins.iter().rev() {
+                let mut counter = 0;
+                while amount - coin >= 0 && amount != 0 && amount >= coin {
+                    amount -= coin;
+                    counter += 1;
+                }
+                if counter >= 1 {
+                    write!(output_file, "{}x{}", counter, coin).expect("Error writing to file");
+                    if amount != 0 {
+                        write!(output_file, " ").expect("Error writing to file");
+                    }
+                }
+                if (amount) == 0 {
+                    writeln!(output_file, "").expect("Error writing to file");
+                    break;
                 }
             }
         }
-
-        // Writing the fewest number of coins needed for each amount to the output file
-        writeln!(
-            output_file,
-            "{}",
-            fewest_coins
-                .iter()
-                .map(|&x| x.to_string())
-                .collect::<Vec<String>>()
-                .join(" ")
-        )
-        .expect("Error writing to file");
     }
 }
